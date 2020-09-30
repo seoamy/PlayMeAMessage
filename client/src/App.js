1122832d0a8c2542795e52eaecb12801130692c1
Amy <amyseo@ucla.edu>
2020-09-30 01:11:12 -0700

    change home page background color

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,24 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import CreatePlaylistView from "./views/CreatePlaylistView"
-import { Emoji, Button, Container, Title, Text } from "./styleComponents"
+import { Emoji, Button, Container, Text } from "./styleComponents"
 
 
 class App extends React.Component {
-
   render() {
     return (
       <div style={{
-        backgroundColor: "#CCF3FF",
+        backgroundColor: "#EA6654",
         width: "100%",
         height: "100vh"
       }}
         className='App' >
+        <a href="/"><img src={require("./music-home-logo.png")} /></a>
         <Router>
           <Switch>
             <Route exact path="/">
               <Container>
-                <Title>Play me a message.</Title>
+                <img src={require("./playMeAMessage.png")} />
                 <Text>We will curate a playlist with song titles that match each word in your message. Perfect for gifting someone a playlist <Emoji symbol="❤️"></Emoji></Text>
                 <Button>
                   <a href='http://localhost:8888/login'> Login to Spotify </a>
