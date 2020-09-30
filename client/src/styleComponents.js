6441296cf965acbed4333177d116a2d9f552849f
Amy <amyseo@ucla.edu>
2020-09-30 01:09:15 -0700

    changed styled components to use the Karla font and changed color theme

--- client/src/styleComponents.js
+++ client/src/styleComponents.js
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import React from "react"
 
+
 const Emoji = props => (
   <span
     className="emoji"
@@ -14,15 +15,16 @@ const Emoji = props => (
 
 const Button = styled("button")`
     border: none;
-    background-color: #FF6D90;
+    background-color: #3E2D2D;
     width: 300px;
     height: 70px;
     border-radius: 50px;
     font-size: 25px;
-    box-shadow: 5px 7px 3px #8888;
+    font-family: 'Karla', sans-serif;
+    box-shadow: 5px 7px 10px #403D3D;
     transition: 0.25s all ease;
     &:hover {
-      background-color: #FF4A76;
+      /* background-color: #; */
       font-size: 27px;
       cursor: pointer
     }
@@ -30,7 +32,7 @@ const Button = styled("button")`
 
 const Container = styled("div")`
     padding: 20px;
-    width: 50%;
+    width: 100%;
     color: black;
     display: flex;
     flex-direction: column;
@@ -40,26 +42,40 @@ const Container = styled("div")`
 
 
 const Title = styled("div")`
-      font-weight: 800;
-      font-family: 'Poppins', sans-serif;
+      font-weight: 1000;
+      font-family: 'Karla', sans-serif;
       color: black;
-      font-size: 40px;
-      padding: 20px;
+      font-size: 48px;
+      /* padding-top: 5px; */
+      /* padding-top: 25px; */
+      padding-bottom: 3vh;
   `;
 
 const Text = styled("div")`
     font-family: "Open Sans";
     font-size: 16px;
-    width: 70%;
+    width: 35%;
     padding-bottom: 30px;
     text-align: center;
+    color: white;
   `;
 
+const InputTitle = styled("div")`
+  font-family: "Karla", sans-serif;
+  font-size: 18px;
+  color: white;
+  padding-bottom: 1vh;
+
+`;
 
 const Input = styled("input")`
   height: 25px;
   width: 100%;
-  font-size: 20px;
+  font-size: 18px;
+  border-radius: 1vh;
+  border: none;
+  box-shadow: 2px 4px 10px #403D3D;
+  margin-bottom: 2.5vh;
 `;
 
-export { Emoji, Button, Container, Title, Text, Input }
+export { Emoji, Button, Container, Title, Text, InputTitle, Input }
